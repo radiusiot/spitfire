f5dbacee50795ccf78690cd17dbd0ee35ff3e4da
Eric Charles <eric@datalayer.io>
2018-01-03 13:23:36 +0100

    fix compilation issue

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/socket/Message.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/socket/Message.java
@@ -185,7 +185,7 @@ public class Message implements JsonSerializable {
     RUN_PARAGRAPH_USING_SPELL,     // [s-c] run paragraph using spell
     PARAS_INFO,                    // [s-c] paragraph runtime infos
     SAVE_FLOWS,
-    LIST_FLOWS
+    LIST_FLOWS,
     SAVE_NOTE_FORMS,              // save note forms
     REMOVE_NOTE_FORMS             // remove note forms
   }
